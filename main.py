d602fd1ac9973fc3dc769cd57f5fdc299f1ce863
Nicolay Barkovsky <barkolia@ya.ru>
2019-08-14 17:25:18 +0300

    changed logic

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-import _requests as r
+import requests as r
 import configparser
 import json
 
 vklink = 'https://api.vk.com/method/'
-
-
 
 # Создадим класс соединения для хранения нужных данных.
 class Connection():
     def __init__(self, filename):
         buf = configparser.ConfigParser()
         buf.read(filename)
+        # Сюда можно добавить еще каких-либо параметров
         self.token = buf['CONFIG']['api_token']
 
     def _req(self, method, data):
-        if
         response = r.get(vklink + str(method),
                          params={**data, 'v':'5.0', 'access_token': self.token})
         response = response.json()
@@ -23,37 +21,29 @@ class Connection():
         else:
             return response['response']
 
-    def newuser(self, id):
-        if type(id) == str:
-            print('detected domain id')
-            response = _req('users.get', {'user_ids': id})[0]
-            return User(response['id'], data=response)
+class User():
+    domain = None
+    uid = None
+    def __init__(self, master, uid, verbose=True):
+        self.conn = master
+        if type(uid) == str:
+            self.domain = uid
+        elif type(uid) == int:
+            self.uid = uid
+        if verbose:
+            self._obtain()
 
-    # MAKE !!COUNTER MORE THAN OFFSET!!
+    def _obtain(self):
+        if self.uid:
+            raw = self.conn._req('users.get', {'user_ids': self.uid})[0]
+        elif self.domain:
+            raw = self.conn._req('users.get', {'user_ids': self.domain})[0]
+        else: raise BaseException('WTF?')
+        if raw:
+            self.__dict__ = {**self.__dict__, **self.raw}
+    @property
+    def friends(self):
 
-class User(Connection):
-    def __init__(self, id, lazy=False, data=None):
-        self.id = id
-        if not lazy:
-            if data:
-                self.fname = data['first_name']
-                self.sname = data['last_name']
-                self.closed = data['is_closed']
-
-        else:
-            self.fname = 'Not'
-            self.closed = False
-            self.sname = 'Obtained'
-            self.friends = []
-
-    def __repr__(self):
-        return('User %i: %s %s' % (self.id, self.fname, self.sname))
-
-class Group(Connection):
-    def __init__(self, id, lazy=False):
-        self.id = id
-        self.name =
-        self.members = []
 
     def __repr__(self):
-        return('User %i: %s %s' % (self.id, self.fname, self.sname))
+        return '<User {} {}>'.format(self.first_name, self.last_name)
